0d338fa6a1820b539ee5b3ac7966a86c5fe747dc
Valentin Diehl <valentindiehl@googlemail.com>
2016-12-11 16:14:35 +0100

    Removed build folder

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/valentin/catkin_ws/src/roboy_reflexes
-# Build directory: /home/valentin/catkin_ws/src/roboy_reflexes/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
