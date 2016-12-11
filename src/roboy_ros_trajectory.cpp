a7407d92b5614e745841dc377ef749a5b898ba7e
Valentin Diehl <valentindiehl@googlemail.com>
2016-12-11 17:25:15 +0100

    update

--- src/roboy_ros_trajectory.cpp
+++ src/roboy_ros_trajectory.cpp
@@ -1,7 +1,11 @@
 #include "roboy_ros_trajectory.hpp"
-#include "../include/flexrayusbinterface/FlexRayHardwareInterface.hpp"
+#include "ros/ros.h"
+#include "std_msgs/Float32.h"
 
-FlexRayHardwareInterface flexray;
+    double static critical_knee_left = 0.02454;
+    double static displacement_val_before = 0;
+    int id = 0;
+Roboy_Ros_Trajectory roboy_ros_trajectory;
 
 Roboy_Ros_Trajectory::Roboy_Ros_Trajectory() {
     if (!ros::isInitialized()) {
@@ -84,15 +88,21 @@ bool Roboy_Ros_Trajectory::initializeMotors(vector<int8_t> &motors, vector<uint8
     return initialize_srv.call(msg);
 }
 
-double readDisplacementSensorLeft(void) {
-  flexray.exchangeData();
-  return flexray.GanglionData[0].muscleState[2].tendonDisplacement / 32768.0f;
-}
+void callback(const std_msgs::Float32::ConstPtr& msgs) {
 
-double readDisplacementSensorRight(void) {
-  flexray.exchangeData();
-  return flexray.GanglionData[2].muscleState[2].tendonDisplacement / 32768.0f;
-}
+        ROS_INFO_STREAM(msgs->data);
+      
+        if(msgs->data <= critical_knee_left && displacement_val_before < msgs->data)
+        {
+            ROS_INFO("Detected critical position. Play motion and move back to stable position");
+            
+            // we send a play steering message
+            roboy_ros_trajectory.playBehaviour(id);
+            ROS_INFO("Robot should be stabilized!. Congrats!");
+            ROS_FATAL("Congrats");
+        }
+        displacement_val_before = msgs->data;
+    }
 
 int main(int argc, char *argv[]) {
     ros::init(argc, argv, "roboy_ros_trajectory");
@@ -100,11 +110,11 @@ int main(int argc, char *argv[]) {
     // Two critical displacement motor knee values
     // You can adjust these.
     
-    double critical_knee_left = 0.02454;
-    double critical_knee_right = 0.0199758;
-    double displacement_val_before = 0;
 
-    Roboy_Ros_Trajectory roboy_ros_trajectory;
+    double critical_knee_right = 0.0199758;
+
+
+    
     // the behaviour we want to play is called test and located in ~/.roboy_gui/behaviours
     roboy_ros_trajectory.loadBehaviour("standupup");
     // it uses 16 motors
@@ -117,37 +127,23 @@ int main(int argc, char *argv[]) {
     // first we need to initialize the ros_control position controllers via a service call
     if (roboy_ros_trajectory.initializeMotors(motors, controlmode)) {
         // when this was successful, the requested position controllers are running and waiting for trajectories
-        int id = roboy_ros_trajectory.behaviours.begin()->first;
+        id = roboy_ros_trajectory.behaviours.begin()->first;
         // we upload the trajectories to the position controllers, who process them
         roboy_ros_trajectory.uploadBehaviour(id);
         
     ROS_INFO("Now start to listen over FlexRay to dispacement data");
-        
-    flexray.initForceControl();
     
     ROS_INFO("All motors in force mode. ");
     
     ROS_INFO("Legs will be moving now. ");
     
-    while(true)
-    {
-        double displacement1 = readDisplacementSensorLeft();
-	double displacement2 = readDisplacementSensorRight();
-	ROS_INFO("dis left" + displacement1);
-	ROS_INFO("dis right" + displacement2);
-        ROS_INFO("dis before" + displacement_val_before);
-      
-        if(displacement1 <= critical_knee_left && displacement2 <= critical_knee_right && displacement_val_before < displacement1)
-        {
-            ROS_INFO("Detected critical position. Play motion and move back to stable position");
-            
-            // we send a play steering message
-            roboy_ros_trajectory.playBehaviour(id);
-            ROS_INFO("Robot should be stabilized!. Congrats!");
-	    break;
-        }
-        displacement_val_before = displacement1;
-    }
+        ros::init(argc, argv, "listener");
+
+	ros::NodeHandle n;
+
+	ros::Subscriber sub = n.subscribe("/roboy/ganglion0/motor2/displacement", 1000, &callback);
+
+	ros::spin();
     
     } else {
         ROS_ERROR("failed to initialize motors, did you launch roboy?\n $ roslaunch roboy_hardware roboy.launch");
